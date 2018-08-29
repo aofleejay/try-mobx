c792af67df65f43dcd231a73334619cc9aa2c674
aofleejay <aofleejay@hotmail.com>
2018-08-29 22:24:59 +0700

    Add MobX debugger

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Devtools from 'mobx-react-devtools'
 import Counter from './Counter'
 import CounterStore from '../stores/Counter'
 
 const App = () => (
-  <Counter counterStore={new CounterStore()} />
+  <Fragment>
+    <Counter counterStore={new CounterStore()} />
+    <Devtools />
+  </Fragment>
 )
 
 export default App
